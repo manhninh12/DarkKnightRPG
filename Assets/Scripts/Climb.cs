4e214021ce2ff842cd846521606e51f7be6fef66
Ninh Mạnh <thienhanh1405@gmail.com>
2026-07-05 22:10:20 +0700

    done climb

--- Assets/Scripts/Climb.cs
+++ Assets/Scripts/Climb.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Climb : MonoBehaviour
 {
+    public float climbSpeed = 5f;
+    private bool isClimbing = false;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Ladder"))
+        {
+            isClimbing = true;
+            rb.gravityScale = 0f; // Disable gravity while climbing
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+            rb.gravityScale = 1f; // Re-enable gravity when not climbing
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, verticalInput * climbSpeed);
+        }
     }
 }
