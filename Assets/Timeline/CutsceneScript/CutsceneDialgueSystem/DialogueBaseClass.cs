8027472565a961d134e70416e999a93c5ecfb92b
Taiphong2006 <tphphong1234@gmail.com>
2026-08-02 22:07:02 +0700

    keep undate

---
+++ Assets/Timeline/CutsceneScript/CutsceneDialgueSystem/DialogueBaseClass.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        private IEnumerator WriteText(string input, Text textHolder)
+        {
+            
+        }
+    }
+}
