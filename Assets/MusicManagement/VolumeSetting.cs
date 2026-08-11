992f99ea756453c17d919d8c4c953636bf5bf8a2
ngphuc-ict <ngphuc1010.ict@gmail.com>
2026-08-12 00:42:57 +0700

    [Debug] Music's setting doesn't apply in Sample Scene

--- Assets/MusicManagement/VolumeSetting.cs
+++ Assets/MusicManagement/VolumeSetting.cs
@@ -9,13 +9,32 @@ public class VolumeSetting : MonoBehaviour
 
     private void Start()
     {
-        // Set the initial volume based on the slider value
-        SetMusicVolume();
+        // Kiểm tra xem người chơi đã từng chỉnh âm lượng chưa
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume(); // Sử dụng giá trị mặc định của Slider
+        }
     }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
+
+        // Quy đổi giá trị Slider (0.0001 -> 1) sang dải Decibel của Audio Mixer (-80dB -> 0dB)
         myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+
+        // Lưu mức âm lượng vào máy
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        // Lấy âm lượng đã lưu và gán lại vào Slider
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        SetMusicVolume();
     }
 }
