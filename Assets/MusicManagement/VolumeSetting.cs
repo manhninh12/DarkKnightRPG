08d0291d7f827663774123dd06af8805aaf0327f
ngphuc-ict <ngphuc1010.ict@gmail.com>
2026-08-11 23:33:52 +0700

    [Feature] Implement music volume setting to the Main Menu scene

---
+++ Assets/MusicManagement/VolumeSetting.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+    private void Start()
+    {
+        // Set the initial volume based on the slider value
+        SetMusicVolume();
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
+}
